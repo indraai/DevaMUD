29d9e9a085d0bff6bfda0618f30a614f27ffaee0
Quinn <quinn@indra.ai>
2021-03-14 08:01:12 -0700

    updating score format

--- dm-dist-alfa/act.informative.c
+++ dm-dist-alfa/act.informative.c
@@ -743,7 +743,7 @@ void do_score(struct char_data *ch, char *argument, int cmd)
 		send_to_char("You are hungry.\n\r", ch);
 
 	sprintf(buf,
-		"hit: %d/%d\n\rmana: %d/%d\n\rmove: %d/%d\n\r",
+		"\n\r# Score\n\rhit: %d/%d\n\rmana: %d/%d\n\rmove: %d/%d\n\r",
 		GET_HIT(ch),GET_MAX_HIT(ch),
 		GET_MANA(ch),GET_MAX_MANA(ch),
 		GET_MOVE(ch),GET_MAX_MOVE(ch));
