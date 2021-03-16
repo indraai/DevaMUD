43c552dd0e69c9c842859b13a36161f8301b37b1
Quinn <quinn@indra.ai>
2021-03-16 07:17:34 -0700

    cleanup

--- dm-dist-alfa/act.wizard.c
+++ dm-dist-alfa/act.wizard.c
@@ -274,11 +274,8 @@ void do_goto(struct char_data *ch, char *argument, int cmd)
 	char_to_room(ch, location);
 	act("$n appears with an ear-splitting bang.", FALSE, ch, 0,0,TO_ROOM);
 	do_look(ch, "",15);
-	// added do where when doing a simple move
-	do_where(ch,"\0",15);
 	// added do exits so it will auto trigger exits on move
-	do_exits(ch,"\0",15);
-
+	o_exits(ch,"\0",15);
 }
 
 
