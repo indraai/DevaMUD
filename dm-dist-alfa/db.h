18fe2e3cfeb63e63fd732efcaa92ad0478de0b4d
Quinn <quinn@indra.ai>
2021-03-18 15:24:53 -0700

    testing

--- dm-dist-alfa/db.h
+++ dm-dist-alfa/db.h
@@ -52,7 +52,10 @@ struct char_data *read_mobile(int nr, int type);
 
 
 #define MENU \
-"\nmenu[Enter]:@mud > 1 \n\r\
+"\n\
+# DEVA.WORLD \n\r\
+=\n\r\
+menu[Enter]:@mud > 1 \n\r\
 menu[Password]:@mud > 4 \n\r\
 menu[Description]:@mud > 2 \n\r\
 menu[Story]:@mud > 3 \n\r\
